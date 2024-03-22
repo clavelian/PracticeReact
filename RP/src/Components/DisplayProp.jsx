e4309361250afc6ceb25da72f054ffd8efce5f66
Clavelian <ianclavel1991@gmail.com>
2024-03-22 21:16:59 +0800

    del: unused component add: defaultvalues for Student.jsx

---
+++ RP/src/Components/DisplayProp.jsx
@@ -0,0 +1,14 @@
+import Student from './Student'
+
+const DisplayProp = () => {
+    return (  
+        <header>
+            <h1>
+              <Student name="Spongebob" age={30} isStudent={true}/>
+
+            </h1>
+        </header>
+    );
+}
+ 
+export default DisplayProp;
