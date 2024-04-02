1102bbfd3098fe67c639397fd0716ebefb012140
Clavelian <ianclavel1991@gmail.com>
2024-04-03 05:34:03 +0800

    Trial

--- RP/src/Components/DigitalClock.jsx
+++ RP/src/Components/DigitalClock.jsx
@@ -1,11 +1,30 @@
-import React, {useState,useEffect} from "react";
+import React, {useState,useEffect, useCallback} from "react";
 
 const DigitalClock = () => {
+
+    const [time,setTime] = useState(new Date())
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    },[])
+
+    const formatTime = () =>{
+        let hours = time.getHours()
+        const minutes = time.getMinutes()
+        const seconds = time.getSeconds()
+        const meridiem = hours >= 12 ? "PM" : "AM"
+    }
 
     return (  
         <>
             <div className="clock-container">
-                <span>00:00:00</span>    
+                <span>00:00:00</span>
             </div>    
         </>
     );
