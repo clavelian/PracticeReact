d5a18836a78577bf60cbdeda4061cf4e0e804b6c
Ian Clavel <ianclavel1991@gmail.com>
2024-07-22 22:59:49 +0800

    d ko alam bakit may commit

--- RP/src/Components/Greeting.jsx
+++ RP/src/Components/Greeting.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function Greeting() {
   const [greeting, setGreeting] = useState('');
@@ -31,5 +31,5 @@ function Greeting() {
     </div>
   );
 }
-
+ 
 export default Greeting;
