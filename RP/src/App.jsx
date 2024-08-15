cf5a0237c839479a52e6b594c859d4436323daac
Ian Clavel <ianclavel1991@gmail.com>
2024-08-15 22:53:36 +0800

    Update App.jsx

--- RP/src/App.jsx
+++ RP/src/App.jsx
@@ -3,32 +3,32 @@ import { useEffect, useState } from 'react'
 import './App.css'
 // // import reactLogo from './assets/react.svg'
 // // import viteLogo from '/vite.svg'
-// import CounterCounter from './Components/CounterCounter.jsx'
-// import Counter from './Components/Counter.jsx'
-// import Student from './UnusedComponents/Student.jsx'
+import CounterCounter from './Components/CounterCounter.jsx'
+import Counter from './Components/Counter.jsx'
+import Student from './UnusedComponents/Student.jsx'
 import Kumpanya from './Components/Kumpanya.jsx'
 import AddDataItem from './Components/addLocalStorage.jsx'
 import DisplayData from './Components/DisplayLocalStorage.jsx'
 // import MyComponent from './Components/MyComponent.jsx'
 // import OtherComponent from './Components/OtherComponent.jsx'
-import Demo from './Components/UseLocalStorage.jsx'
-import DigitalClock from './Components/DigitalClock.jsx'
-import Greeting from './Components/Greeting.jsx'
+// import Demo from './Components/UseLocalStorage.jsx'
+// import DigitalClock from './Components/DigitalClock.jsx'
+// import Greeting from './Components/Greeting.jsx'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  // const handleAdd = () => setCount(count + 1)
-  // const handleMulti = () => setCount(count * 2)
-  // const handleSub = () => setCount((count) > 0 ? count - 1 : count)
-  // const handleDiv = () => setCount(count > 0 ? Math.ceil(count / 2) : count)
-  // const handleReset = () => setCount(0)
+  const [count, setCount] = useState(0)
+  const handleAdd = () => setCount(count + 1)
+  const handleMulti = () => setCount(count * 2)
+  const handleSub = () => setCount((count) > 0 ? count - 1 : count)
+  const handleDiv = () => setCount(count > 0 ? Math.ceil(count / 2) : count)
+  const handleReset = () => setCount(0)
                           
-                          // setCount((count) > 0 ? count - 1 : count)
+                          setCount((count) > 0 ? count - 1 : count)
 
 
   return (
     <>
-        {/* <button onClick={(handleSub)}>-</button>
+        <button onClick={(handleSub)}>-</button>
           count is {count}
         <button onClick={(handleMulti)}>x</button>
         <button onClick={(handleAdd)}>+</button>
@@ -38,7 +38,7 @@ function App() {
         <br/>
         <CounterCounter/>
         </div>
-        <Counter/> */}
+        <Counter/>
         {/* <br/>
         <br/> */}
         {/* <Student name="Spongebob" age={30} isStudent={true}  /> */}
@@ -48,18 +48,18 @@ function App() {
 
         {/* <h1>This text must be replaced after the Company Name has been changed</h1> */}
         
-        <Kumpanya />
+        {/* <Kumpanya />
 
         <AddDataItem />
-        <DisplayData />
+        <DisplayData /> */}
         {/* <MyComponent /> */}
         {/* <OtherComponent  /> */}
-        <Demo/>
+        {/* <Demo/>
         <br />
         <br />
         <br />
         <DigitalClock/>
-        <Greeting />
+        <Greeting /> */}
     </>
   )
 }
